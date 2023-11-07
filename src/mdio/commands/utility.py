267df52afa0dde29972941d10a5f2fee48df5e12
Mark Roberts <mark.roberts@tgs.com>
2023-11-07 16:51:35 -0600

    Fix mdio copy.

--- src/mdio/commands/utility.py
+++ src/mdio/commands/utility.py
@@ -27,9 +27,19 @@ except SystemError:
     type=click.STRING,
 )
 @click.option(
+    "-access",
+    "--access-pattern",
+    required=False,
+    default="012",
+    help="Access pattern of the file",
+    type=click.STRING,
+    show_default=True,
+)
+@click.option(
     "-exc",
     "--excludes",
     required=False,
+    default="",
     help="""Data to exclude during copy. i.e. chunked_012. The raw data won’t be
     copied, but it will create an empty array to be filled. If left blank, it will
     copy everything.""",
@@ -39,6 +49,7 @@ except SystemError:
     "-inc",
     "--includes",
     required=False,
+    default="",
     help="""Data to include during copy. i.e. trace_headers. If this is not
     specified, and certain data is excluded, it will not copy headers. To
     preserve headers, specify trace_headers. If left blank, it will copy
@@ -62,8 +73,9 @@ except SystemError:
     show_default=True,
 )
 def copy(
-    input_mdio_path,
+    input_mdio_path: str,
     output_mdio_path: str,
+    access_pattern: str = "012",
     includes: str = "",
     excludes: str = "",
     storage_options: Optional[dict] = None,
@@ -77,8 +89,11 @@ def copy(
     More documentation about `excludes` and `includes` can be found
     in Zarr's documentation in `zarr.convenience.copy_store`.
     """
+    reader = mdio.MDIOReader(
+        input_mdio_path, access_pattern=access_pattern, return_metadata=True
+    )
     mdio.copy_mdio(
-        source=input_mdio_path,
+        source=reader,
         dest_path_or_buffer=output_mdio_path,
         excludes=excludes,
         includes=includes,
