07bc1e3fc4eb159ac139807d6b67dc37abbe4498
Mark Roberts <mark.roberts@tgs.com>
2023-11-07 16:51:35 -0600

    Fix mdio copy.

--- noxfile.py
+++ noxfile.py
@@ -132,6 +132,7 @@ def precommit(session: Session) -> None:
         "pre-commit",
         "pre-commit-hooks",
         "pyupgrade",
+        "pytest-dependency",
     )
     session.run("pre-commit", *args)
     if args and args[0] == "install":
