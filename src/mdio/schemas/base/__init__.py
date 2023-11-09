0c4eebc95838e3a2ff4206f20c1d3645efba2828
Altay Sansal <tasansal@users.noreply.github.com>
2023-11-09 17:30:20 -0600

    Refactor import paths in base schema
    The base schema's import paths for `DataType`, `StructuredDataType`, `UserAttributes`, and `Dimension` have been updated. This change involves updating the original source from `numeric` to `scalar` and adding `metadata` and `dimension` paths.

--- src/mdio/schemas/base/__init__.py
+++ src/mdio/schemas/base/__init__.py
@@ -4,8 +4,10 @@
 from typing import TypeAlias
 
 from mdio.schemas.base.blosc import Blosc
-from mdio.schemas.base.numeric import DataType
-from mdio.schemas.base.numeric import StructuredDataType
+from mdio.schemas.base.dimension import Dimension
+from mdio.schemas.base.metadata import UserAttributes
+from mdio.schemas.base.scalar import DataType
+from mdio.schemas.base.scalar import StructuredDataType
 from mdio.schemas.base.zfp import ZFP
 
 
@@ -14,6 +16,8 @@ Compressors: TypeAlias = Blosc | ZFP
 
 __all__ = [
     "Compressors",
+    "Dimension",
+    "UserAttributes",
     "DataType",
     "StructuredDataType",
 ]
