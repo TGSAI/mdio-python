17091bbd0878c220907847d8b96c6b6fb3a79ad2
Altay Sansal <tasansal@users.noreply.github.com>
2023-11-09 17:22:01 -0600

    Extend Dimension data model in mdio schema
    The Dimension class in the mdio schema has been updated to not only include 'name' and 'size' fields with their respective descriptions, but also to introduce a new optional field 'chunksize'. This update enhances the data model, providing a clearer definition of a Dimension object and allowing for chunking specification.

--- src/mdio/schemas/base/dimension.py
+++ src/mdio/schemas/base/dimension.py
@@ -5,7 +5,19 @@ from pydantic import Field
 
 
 class Dimension(BaseModel):
-    """Data model for a dimension."""
+    """Data model defining a Dimension object."""
 
-    name: str = Field(description="Name of the dimension.")
-    size: int = Field(description="Size of the dimension.")
+    name: str = Field(
+        ...,
+        description="Name of the dimension.",
+    )
+    size: int = Field(
+        ...,
+        gt=0,
+        description="Size of the dimension.",
+    )
+    chunksize: int | None = Field(
+        default=None,
+        gt=0,
+        description="Chunk size of the dimension.",
+    )
