62dd68afd9b9be46a43942ba71be687b6699bb48
Altay Sansal <tasansal@users.noreply.github.com>
2023-11-29 16:58:33 -0600

    Refactor Dimension class in dimension.py
    Dimension class now extends from StrictCamelBaseModel instead of BaseModel. This change allows enforcing more strict camel-case convention for attribute names. Additionally, TypeAlias has been used for DimensionCollection, DimensionReference and DimensionContext to improve code readability and maintainability.

--- src/mdio/schemas/base/dimension.py
+++ src/mdio/schemas/base/dimension.py
@@ -1,23 +1,27 @@
 """Dimension schema."""
 
-from pydantic import BaseModel
+
+from typing import TypeAlias
+
 from pydantic import Field
 
+from mdio.schemas.base.core import StrictCamelBaseModel
 
-class Dimension(BaseModel):
-    """Data model defining a Dimension object."""
 
-    name: str = Field(
-        ...,
-        description="Name of the dimension.",
+class Dimension(StrictCamelBaseModel):
+    """Represents a single dimension with a name and size.
+
+    Optionally includes a chunk size for partitioning the dimension.
+    """
+
+    name: str = Field(..., description="Unique identifier for the dimension.")
+    size: int = Field(..., gt=0, description="Total size of the dimension.")
+    chunk_size: int | None = Field(
+        default=None, gt=0, description="Optional partition size for the dimension."
     )
-    size: int = Field(
-        ...,
-        gt=0,
-        description="Size of the dimension.",
-    )
-    chunksize: int | None = Field(
-        default=None,
-        gt=0,
-        description="Chunk size of the dimension.",
-    )
+
+
+DimensionCollection: TypeAlias = list[Dimension]
+DimensionReference: TypeAlias = list[str]
+
+DimensionContext: TypeAlias = list[Dimension] | list[str]
