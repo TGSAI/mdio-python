dc41203fbc02cf5c2a5a35694c3c2bbe6e77030d
Altay Sansal <tasansal@users.noreply.github.com>
2023-11-29 17:08:10 -0600

    Remove unused imports from base __init__.py
    This commit removes Dimension, UserAttributes, DataType, and StructuredDataType imports from base __init__.py file. These imports were unused, removing them improves the code cleanliness and readability.

--- src/mdio/schemas/base/__init__.py
+++ src/mdio/schemas/base/__init__.py
@@ -4,10 +4,6 @@
 from typing import TypeAlias
 
 from mdio.schemas.base.blosc import Blosc
-from mdio.schemas.base.dimension import Dimension
-from mdio.schemas.base.metadata import UserAttributes
-from mdio.schemas.base.scalar import DataType
-from mdio.schemas.base.scalar import StructuredDataType
 from mdio.schemas.base.zfp import ZFP
 
 
@@ -16,8 +12,4 @@ Compressors: TypeAlias = Blosc | ZFP
 
 __all__ = [
     "Compressors",
-    "Dimension",
-    "UserAttributes",
-    "DataType",
-    "StructuredDataType",
 ]
