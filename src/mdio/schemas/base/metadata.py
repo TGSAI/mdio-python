af0be44f1acb7d3ea7ee22be09efa3abe1bf5edc
Altay Sansal <tasansal@users.noreply.github.com>
2023-11-29 16:58:53 -0600

    Refactor mdio metadata schema classes
    Several classes in the mdio metadata schema have been refactored to inherit from StrictCamelBaseModel instead of BaseModel, implementing stricter camel case conventions. Additionally, validation model and dump methods are simplified by leveraging inherent features of StrictCamelBaseModel. The UserAttributes class now handles "None" attributes, and a new MetadataContainer class is added.

--- src/mdio/schemas/base/metadata.py
+++ src/mdio/schemas/base/metadata.py
@@ -4,10 +4,9 @@
 import re
 from typing import Any
 
-from pydantic import BaseModel
-from pydantic import ConfigDict
 from pydantic import Field
-from pydantic import model_validator
+
+from mdio.schemas.base.core import StrictCamelBaseModel
 
 
 def follows_metadata_key_convention(key: str) -> bool:
@@ -40,48 +39,26 @@ def follows_metadata_key_convention(key: str) -> bool:
     return bool(re.match(pattern, key))
 
 
-class VersionedMetadataConvention(BaseModel):
+class VersionedMetadataConvention(StrictCamelBaseModel):
     """Data model for versioned metadata convention."""
-
-    model_config = ConfigDict(
-        extra="ignore",
-        str_to_lower=True,
-        populate_by_name=True,
-        frozen=True,
-        alias_generator=lambda x: x.replace("_", "-"),
-    )
-
-    @model_validator(mode="after")
-    def check_key_and_alias(self):
-        """Checks if key and alias matches expected patterns."""
-        if len(self.model_fields) != 1:
-            raise ValueError(f"{self.__class__.__name__} can only have one field.")
-
-        name = self.__class__.__name__
-        key = next(iter(self.model_fields))
-
-        if not follows_metadata_key_convention(key):
-            raise ValueError(
-                f"The provided key for {name} does not follow the required "
-                "naming convention. Please ensure the key is formatted as "
-                "'name_v1'. For instance, use 'unit_v1'."
-            )
-
-        return self
 
     def model_dump(self, *args, **kwargs) -> dict[str, Any]:
         """Override default model dump to use alias."""
-        return super().model_dump(by_alias=True, *args, **kwargs)
+        return super().model_dump(*args, **kwargs, by_alias=True)
 
     def model_dump_json(self, *args, **kwargs) -> str:
         """Override default model dump json to use alias."""
-        return super().model_dump_json(by_alias=True, *args, **kwargs)
+        return super().model_dump_json(*args, **kwargs, by_alias=True)
 
 
-class UserAttributes(BaseModel):
+class UserAttributes(StrictCamelBaseModel):
     """User defined attributes as key/value pairs."""
 
-    attributes: dict[str, Any] = Field(
-        default_factory=dict,
+    attributes: dict[str, Any] | None = Field(
+        default=None,
         description="User defined attributes as key/value pairs.",
     )
+
+
+class MetadataContainer(StrictCamelBaseModel):
+    """A container model for metadata."""
