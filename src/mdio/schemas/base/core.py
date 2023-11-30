e6fdbe10f92533fce7291b27a4c6ff4071b6ebd9
Altay Sansal <tasansal@users.noreply.github.com>
2023-11-30 17:09:02 -0600

    Refactor `model_fields` function for code reusability
    The `model_fields` function, initially present in the `mdio.schemas.v1.variable` module, has been moved to the `mdio.schemas.base.core` module. This refactor allows for better code reusability and also eliminates code duplication.

--- src/mdio/schemas/base/core.py
+++ src/mdio/schemas/base/core.py
@@ -1,6 +1,9 @@
 """This module implements the core components of the MDIO schemas."""
 
 
+from typing import Any
+from typing import get_type_hints
+
 from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import Field
@@ -48,3 +51,30 @@ class RectilinearChunkGrid(StrictCamelBaseModel):
     configuration: RectilinearChunkShape = Field(
         ..., description="Configuration of the irregular chunk grid."
     )
+
+
+def model_fields(model: type[BaseModel]) -> dict[str, tuple[Any, Any]]:
+    """Extract Pydantic BaseModel fields.
+
+    Args:
+        model: (Type) The model object for which the fields will be extracted.
+
+    Returns:
+        A dictionary containing the fields of the model along with
+        their corresponding types and default values.
+
+    Example:
+        >>> class MyModel(BaseModel):
+        ...     name: str
+        ...     age: int = 0
+        ...
+        >>> model_fields(MyModel)
+        {'name': (str, <default_value>), 'age': (int, 0)}
+    """
+    annotations = get_type_hints(model)
+
+    fields = {}
+    for field_name, field in model.model_fields.items():
+        fields[field_name] = (annotations[field_name], field)
+
+    return fields
--- src/mdio/schemas/v1/variable.py
+++ src/mdio/schemas/v1/variable.py
@@ -11,15 +11,11 @@ additional metadata.
 variable in MDIO format. It can have coordinates and can also hold metadata.
 """
 
-
-from typing import Any
-from typing import get_type_hints
-
-from pydantic import BaseModel
 from pydantic import Field
 from pydantic import create_model
 
 from mdio.schemas.base.array import NamedArray
+from mdio.schemas.base.core import model_fields
 from mdio.schemas.base.dtype import ScalarType
 from mdio.schemas.base.dtype import StructuredType
 from mdio.schemas.base.encoding import ChunkGridMetadata
@@ -37,34 +33,6 @@ class Coordinate(NamedArray):
     metadata: list[CoordinateUnits | UserAttributes] | None = Field(
         default=None, description="Coordinate metadata."
     )
-
-
-# Function to extract field types and defaults
-def model_fields(model: type[BaseModel]) -> dict[str, tuple[Any, Any]]:
-    """Extract Pydantic BaseModel fields.
-
-    Args:
-        model: (Type) The model object for which the fields will be extracted.
-
-    Returns:
-        A dictionary containing the fields of the model along with
-        their corresponding types and default values.
-
-    Example:
-        >>> class MyModel(BaseModel):
-        ...     name: str
-        ...     age: int = 0
-        ...
-        >>> model_fields(MyModel)
-        {'name': (str, <default_value>), 'age': (int, 0)}
-    """
-    annotations = get_type_hints(model)
-
-    fields = {}
-    for field_name, field in model.model_fields.items():
-        fields[field_name] = (annotations[field_name], field)
-
-    return fields
 
 
 VariableMetadata = create_model(
