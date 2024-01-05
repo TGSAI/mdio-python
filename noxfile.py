a342745a9afa4ff4f7ca5da349b11caf57066fce
Altay Sansal <tasansal@users.noreply.github.com>
2024-01-05 08:56:09 -0600

    add python3.12 to nox

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 package = "mdio"
-python_versions = ["3.11", "3.10", "3.9"]
+python_versions = ["3.12", "3.11", "3.10", "3.9"]
 nox.needs_version = ">= 2022.1.7"
 nox.options.sessions = (
     "pre-commit",
