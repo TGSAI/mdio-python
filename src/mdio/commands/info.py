ecb1b57601e568d87dc9efc7b3168d667ee9cd28
Altay Sansal <tasansal@users.noreply.github.com>
2024-03-07 12:25:51 -0600

    Refactor MDIO info command for better code organization
    The MDIO info command is refactored to enhance code readability and maintenance. The new structure involves separate functions for 'cast_stats', 'parse_grid' and 'pretty_print' to each perform distinct tasks. This improves the clear segregation of tasks and ease of future modifications.

--- src/mdio/commands/info.py
+++ src/mdio/commands/info.py
@@ -1,18 +1,18 @@
 """MDIO Dataset information command."""
 
 
-from json import dumps as json_dumps
+from typing import TYPE_CHECKING
+from typing import Any
 
 from click import STRING
 from click import Choice
 from click import argument
 from click import command
 from click import option
-from rich import print
-from rich.console import Console
-from rich.table import Table
 
-from mdio import MDIOReader
+
+if TYPE_CHECKING:
+    from mdio.core import Grid
 
 
 @command(name="info")
@@ -47,55 +47,86 @@ def info(
     the dataset. If output-format is set to json then a json is returned to
     facilitate parsing.
     """
+    from mdio import MDIOReader
+
     reader = MDIOReader(
         mdio_path,
         access_pattern=access_pattern,
         return_metadata=True,
     )
 
-    mdio_dict = {}
-    mdio_dict["grid"] = {}
+    grid_dict = parse_grid(reader.grid)
+    stats_dict = cast_stats(reader.stats)
 
-    for dim_name in reader.grid.dim_names:
-        dim = reader.grid.select_dim(dim_name)
+    mdio_info = {
+        "path": mdio_path,
+        "stats": stats_dict,
+        "grid": grid_dict,
+    }
+
+    if output_format == "pretty":
+        pretty_print(mdio_info)
+
+    if output_format == "json":
+        json_print(mdio_info)
+
+
+def cast_stats(stats_dict: dict[str, Any]) -> dict[str, float]:
+    """Normalize all floats to JSON serializable floats."""
+    return {k: float(v) for k, v in stats_dict.items()}
+
+
+def parse_grid(grid: "Grid") -> dict[str, dict[str, int | str]]:
+    """Extract grid information per dimension."""
+    grid_dict = {}
+    for dim_name in grid.dim_names:
+        dim = grid.select_dim(dim_name)
         min_ = str(dim.coords[0])
         max_ = str(dim.coords[-1])
         size = str(dim.coords.shape[0])
-        axis_dict = {"name": dim_name, "min": min_, "max": max_, "size": size}
-        mdio_dict["grid"][dim_name] = axis_dict
+        grid_dict[dim_name] = {"name": dim_name, "min": min_, "max": max_, "size": size}
+    return grid_dict
 
-    if output_format == "pretty":
-        console = Console()
 
-        grid_table = Table(show_edge=False)
-        grid_table.add_column("Dimension", justify="right", style="cyan", no_wrap=True)
-        grid_table.add_column("Min", justify="left", style="magenta")
-        grid_table.add_column("Max", justify="left", style="magenta")
-        grid_table.add_column("Size", justify="left", style="green")
+def json_print(mdio_info: dict[str, Any]) -> None:
+    """Convert MDIO Info to JSON and pretty print."""
+    from json import dumps as json_dumps
 
-        for _, axis_dict in mdio_dict["grid"].items():
-            name, min_, max_, size = axis_dict.values()
-            grid_table.add_row(name, min_, max_, size)
+    from rich import print
 
-        stat_table = Table(show_edge=False)
-        stat_table.add_column("Stat", justify="right", style="cyan", no_wrap=True)
-        stat_table.add_column("Value", justify="left", style="magenta")
+    print(json_dumps(mdio_info, indent=2))
 
-        for stat, value in reader.stats.items():
-            stat_table.add_row(stat, f"{value:.4f}")
 
-        master_table = Table(title=f"File Information for {mdio_path}")
-        master_table.add_column("MDIO Grid", justify="center")
-        master_table.add_column("MDIO Statistics", justify="center")
-        master_table.add_row(grid_table, stat_table)
+def pretty_print(mdio_info: dict[str, Any]) -> None:
+    """Print pretty MDIO Info table to console."""
+    from rich.console import Console
+    from rich.table import Table
 
-        console.print(master_table)
+    console = Console()
 
-    if output_format == "json":
-        stats_cast = {k: float(v) for k, v in reader.stats.items()}
-        mdio_dict["stats"] = stats_cast
+    grid_table = Table(show_edge=False)
+    grid_table.add_column("Dimension", justify="right", style="cyan", no_wrap=True)
+    grid_table.add_column("Min", justify="left", style="magenta")
+    grid_table.add_column("Max", justify="left", style="magenta")
+    grid_table.add_column("Size", justify="left", style="green")
 
-        print(json_dumps(mdio_dict, indent=2))
+    for _, axis_dict in mdio_info["grid"].items():
+        name, min_, max_, size = axis_dict.values()
+        grid_table.add_row(name, min_, max_, size)
+
+    stat_table = Table(show_edge=False)
+    stat_table.add_column("Stat", justify="right", style="cyan", no_wrap=True)
+    stat_table.add_column("Value", justify="left", style="magenta")
+
+    for stat, value in mdio_info["stats"].items():
+        stat_table.add_row(stat, f"{value:.4f}")
+
+    master_table = Table(title=f"File Information for {mdio_info['path']}")
+    master_table.add_column("MDIO Grid", justify="center")
+    master_table.add_column("MDIO Statistics", justify="center")
+    master_table.add_row(grid_table, stat_table)
+
+    console.print(master_table)
 
 
 cli = info
