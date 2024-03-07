fb6af776fb5c392f03a174f5f6a2c7a8c01d47d6
Altay Sansal <tasansal@users.noreply.github.com>
2024-03-07 11:41:29 -0600

    Refactor imports and command options in copy.py

--- src/mdio/commands/copy.py
+++ src/mdio/commands/copy.py
@@ -1,61 +1,62 @@
 """MDIO Dataset copy command."""
 
 
-from typing import Optional
-
-import click
 import click_params
+from click import BOOL
+from click import STRING
+from click import argument
+from click import command
+from click import option
 
-import mdio
+from mdio import MDIOReader
 
 
-@click.command(name="copy")
-@click.argument("source-mdio-path", type=str)
-@click.argument("target-mdio-path", type=str)
-@click.option(
+@command(name="copy")
+@argument("source-mdio-path", type=str)
+@argument("target-mdio-path", type=str)
+@option(
     "-access",
     "--access-pattern",
     required=False,
     default="012",
     help="Access pattern of the file",
-    type=click.STRING,
+    type=STRING,
     show_default=True,
 )
-@click.option(
+@option(
     "-exc",
     "--excludes",
     required=False,
     default="",
-    help="""Data to exclude during copy. i.e. chunked_012. The raw data won’t be
-    copied, but it will create an empty array to be filled. If left blank, it will
-    copy everything.""",
-    type=click.STRING,
+    help="Data to exclude during copy, like `chunked_012`. The data values won’t be "
+    "copied but an empty array will be created. If blank, it copies everything.",
+    type=STRING,
 )
-@click.option(
+@option(
     "-inc",
     "--includes",
     required=False,
     default="",
-    help="""Data to include during copy. i.e. trace_headers. If this is not
-    specified, and certain data is excluded, it will not copy headers. To
-    preserve headers, specify trace_headers. If left blank, it will copy
-    everything except specified in excludes parameter.""",
-    type=click.STRING,
+    help="Data to include during copy, like `trace_headers`. If not specified, and "
+    "certain data is excluded, it will not copy headers. To preserve headers, "
+    "specify trace_headers. If left blank, it will copy everything except what is "
+    "specified in the 'excludes' parameter.",
+    type=STRING,
 )
-@click.option(
+@option(
     "-storage",
     "--storage-options",
     required=False,
     help="Custom storage options for cloud backends",
     type=click_params.JSON,
 )
-@click.option(
+@option(
     "-overwrite",
     "--overwrite",
     required=False,
     default=False,
-    help="Flag to overwrite if mdio file if it exists",
-    type=click.BOOL,
+    help="Flag to overwrite if MDIO file exists",
+    type=BOOL,
     show_default=True,
 )
 def copy(
@@ -64,7 +65,7 @@ def copy(
     access_pattern: str = "012",
     includes: str = "",
     excludes: str = "",
-    storage_options: Optional[dict] = None,
+    storage_options: dict | None = None,
     overwrite: bool = False,
 ):
     """Copy a MDIO dataset to anpther MDIO dataset.
@@ -75,11 +76,9 @@ def copy(
     More documentation about `excludes` and `includes` can be found
     in Zarr's documentation in `zarr.convenience.copy_store`.
     """
-    reader = mdio.MDIOReader(
-        source_mdio_path, access_pattern=access_pattern, return_metadata=True
-    )
-    mdio.copy_mdio(
-        source=reader,
+    reader = MDIOReader(source_mdio_path, access_pattern=access_pattern)
+
+    reader.copy(
         dest_path_or_buffer=target_mdio_path,
         excludes=excludes,
         includes=includes,
