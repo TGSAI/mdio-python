3c6048f94cd0306ca6e2d553a0c5fef4dd2b0281
Altay Sansal <tasansal@users.noreply.github.com>
2024-03-07 11:31:23 -0600

    make tests work for option -> argument conversion

--- tests/test_main.py
+++ tests/test_main.py
@@ -41,9 +41,7 @@ def test_main_info_succeeds(runner: CliRunner, zarr_tmp: Path) -> None:
 @pytest.mark.dependency(depends=["test_main_succeeds"])
 def test_main_copy_succeeds(runner: CliRunner, zarr_tmp: Path, zarr_tmp2: Path) -> None:
     """It exits with a status code of zero."""
-    cli_args = ["copy"]
-    cli_args.extend(["-i", str(zarr_tmp)])
-    cli_args.extend(["-o", str(zarr_tmp2)])
+    cli_args = ["copy", str(zarr_tmp), str(zarr_tmp2)]
 
     result = runner.invoke(__main__.main, args=cli_args)
     assert result.exit_code == 0
