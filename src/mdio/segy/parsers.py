1ddd5df0eb40b357e31beb4f9b97f563fb1dbb0f
Altay Sansal <tasansal@users.noreply.github.com>
2024-03-21 18:06:50 -0500

    Refactor SEG-Y parser and streamline imports
    Refactor the parsing functions in `src/mdio/segy/parsers.py` to simplify the codebase and improve maintainability. Redundant functions such as `parse_binary_header`, `parse_text_header`, and `get_trace_count` have been removed, while imports have been condensed to only essential modules. The `NUM_CORES` logic is updated to count logical cores instead of just physical ones.

--- src/mdio/segy/parsers.py
+++ src/mdio/segy/parsers.py
@@ -5,101 +5,43 @@ from __future__ import annotations
 from concurrent.futures import ProcessPoolExecutor
 from itertools import repeat
 from math import ceil
-from typing import Any
-from typing import Sequence
+from typing import TYPE_CHECKING
 
 import numpy as np
-import segyio
-from numpy.typing import NDArray
 from psutil import cpu_count
 from tqdm.auto import tqdm
 
-from mdio.core import Dimension
 from mdio.segy._workers import header_scan_worker
-from mdio.segy.byte_utils import Dtype
 
 
-NUM_CORES = cpu_count(logical=False)
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+    from segy import SegyFile
 
-
-def get_trace_count(segy_path, segy_endian):
-    """Get trace count from SEG-Y file (size)."""
-    with segyio.open(
-        filename=segy_path,
-        mode="r",
-        ignore_geometry=True,
-        endian=segy_endian,
-    ) as segy_handle:
-        trace_count = segy_handle.tracecount
-
-    return trace_count
-
-
-def parse_binary_header(segy_handle: segyio.SegyFile) -> dict[str, Any]:
-    """Parse `segyio.BinField` as python `dict`.
-
-    The `segyio` library returns the binary header as a `BinField` instance,
-    which is not serializable. Here we parse it as a dictionary, so we can
-    later convert it to JSON.
-
-    Args:
-        segy_handle: The SegyFile instance in context.
-
-    Returns:
-        Parsed binary header key, value pairs.
-    """
-    binary_header = segy_handle.bin
-    return {str(entry): binary_header[entry] for entry in binary_header}
-
-
-def parse_text_header(segy_handle: segyio.SegyFile) -> list[str]:
-    """Parse text header from `bytearray` to python `list` of `str` per line.
-
-    The `segyio` library returns the text header as a  `bytearray` instance.
-     Here we parse it as a list of strings (per line).
-
-    Args:
-        segy_handle: The SegyFile instance in context.
-
-    Returns:
-        Parsed text header in list with lines as elements.
-    """
-    text_header = segy_handle.text[0].decode(errors="ignore")
-    text_header = [
-        text_header[char_idx : char_idx + 80]
-        for char_idx in range(0, len(text_header), 80)
-    ]
-    return text_header
+NUM_CORES = cpu_count(logical=True)
 
 
 def parse_trace_headers(
-    segy_path: str,
-    segy_endian: str,
-    byte_locs: Sequence[int],
-    byte_types: Sequence[Dtype],
-    index_names: Sequence[str],
+    segy_file: SegyFile,
+    index_names: list[str],
     block_size: int = 50000,
     progress_bar: bool = True,
-) -> dict[str, NDArray]:
+) -> NDArray:
     """Read and parse given `byte_locations` from SEG-Y file.
 
     Args:
-        segy_path: Path to the input SEG-Y file.
-        segy_endian: Endianness of the input SEG-Y in {'big', 'little'}.
-        byte_locs: Byte locations to return. It will be a subset of headers.
-        byte_types: Data types of each header key to index. Must be the
-            same count as `byte_lengths`.
-        index_names: Tuple of the names for the index attributes
+        segy_file: SegyFile instance.
+        index_names: Tuple of the names for the index attributes.
         block_size: Number of traces to read for each block.
         progress_bar: Enable or disable progress bar. Default is True.
 
     Returns:
         dictionary with headers:  keys are the index names, values are numpy
             arrays of parsed headers for the current block. Array is of type
-            byte_type with the exception of IBM32 which is mapped to FLOAT32.
+            byte_type except IBM32 which is mapped to FLOAT32.
 
     """
-    trace_count = get_trace_count(segy_path, segy_endian)
+    trace_count = segy_file.num_traces
     n_blocks = int(ceil(trace_count / block_size))
 
     trace_ranges = []
@@ -117,12 +59,9 @@ def parse_trace_headers(
         # pool.imap is lazy
         lazy_work = executor.map(
             header_scan_worker,  # fn
-            repeat(segy_path),
+            repeat(segy_file),
             trace_ranges,
-            repeat(byte_locs),
-            repeat(byte_types),
             repeat(index_names),
-            repeat(segy_endian),
             chunksize=2,  # Not array chunks. This is for `multiprocessing`
         )
 
@@ -137,27 +76,5 @@ def parse_trace_headers(
         # This executes the lazy work.
         headers = list(lazy_work)
 
-    final_headers = {}
-    for header_name in index_names:
-        final_headers[header_name] = np.concatenate(
-            [header[header_name] for header in headers]
-        )
     # Merge blocks before return
-    return final_headers
-
-
-def parse_sample_axis(binary_header: dict) -> Dimension:
-    """Parse sample axis from binary header.
-
-    Args:
-        binary_header: Dictionary representing binary header.
-
-    Returns:
-        MDIO Dimension instance for sample axis.
-    """
-    num_samp = binary_header["Samples"]
-    interval = binary_header["Interval"] // 1000
-
-    max_val = num_samp * interval
-
-    return Dimension(coords=range(0, max_val, interval), name="sample")
+    return np.concatenate(headers)
