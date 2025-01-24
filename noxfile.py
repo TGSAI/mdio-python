3405fb3a41776d86af6eac942ab42949052bbea5
Altay Sansal <tasansal@users.noreply.github.com>
2025-01-24 15:55:33 -0600

    Upgrade CI to support Python 3.13

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ except ImportError:
 
 
 package = "mdio"
-python_versions = ["3.12", "3.11", "3.10"]
+python_versions = ["3.13", "3.12", "3.11", "3.10"]
 nox.needs_version = ">= 2022.1.7"
 nox.options.sessions = (
     "pre-commit",
