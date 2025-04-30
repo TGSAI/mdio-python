d0d446b8f06f3cc5991fe986961c3ebc81e21eb5
Altay Sansal <tasansal@users.noreply.github.com>
2025-04-30 16:37:37 -0500

    Refactor type hints and optimize imports in io_utils.py

--- src/mdio/api/io_utils.py
+++ src/mdio/api/io_utils.py
@@ -2,15 +2,21 @@
 
 from __future__ import annotations
 
-import dask.array as da
-import zarr
+from typing import TYPE_CHECKING
+from typing import Any
+
+from dask.array import from_array
+
+if TYPE_CHECKING:
+    from dask.array import Array as DaskArray
+    from zarr import Array as ZarrArray
+    from zarr import Group as ZarrGroup
 
 
 def process_url(url: str, disk_cache: bool) -> str:
     """Process URL based on options.
 
-    File cache is only valid for remote stores. The LRU caching works
-    on both remote and local.
+    File cache is only valid for remote stores. The LRU caching works on both remote and local.
 
     Args:
         url: FSSpec compliant url
@@ -20,9 +26,8 @@ def process_url(url: str, disk_cache: bool) -> str:
         String to store with augmentations like cache, etc.
 
     Examples:
-        If we want to access an MDIO file from S3 without using disk caching,
-        the simplecache protocol is not used, and therefore we only need to
-        specify the s3 filesystem options:
+        If we want to access an MDIO file from S3 without using disk caching, the simplecache
+        protocol is not used, and therefore we only need to specify the s3 filesystem options:
 
         >>> from mdio.api.convenience import process_url
         >>>
@@ -33,12 +38,12 @@ def process_url(url: str, disk_cache: bool) -> str:
         ... )
     """
     if disk_cache is True:
-        url = "::".join(["simplecache", url])
+        url = f"simplecache::{url}"
 
     return url
 
 
-def open_zarr_array(group_handle: zarr.Group, name: str) -> zarr.Array:
+def open_zarr_array(group_handle: ZarrGroup, name: str) -> ZarrArray:
     """Open Zarr array lazily using Zarr.
 
     Note: All other kwargs are ignored, used for API compatibility for dask backend.
@@ -53,7 +58,7 @@ def open_zarr_array(group_handle: zarr.Group, name: str) -> zarr.Array:
     return group_handle[name]
 
 
-def open_zarr_array_dask(group_handle: zarr.Group, name: str, **kwargs) -> da.Array:
+def open_zarr_array_dask(group_handle: ZarrGroup, name: str, **kwargs: Any) -> DaskArray:  # noqa: ANN401
     """Open Zarr array lazily using Dask.
 
     Note: All other kwargs get passed to dask.array.from_zarr()
@@ -61,10 +66,10 @@ def open_zarr_array_dask(group_handle: zarr.Group, name: str, **kwargs) -> da.Ar
     Args:
         group_handle: Group handle where the array is located
         name: Name of the array within the group
-        **kwargs: Extra keyword arguments for Dask from_zarr.  # noqa: RST210
+        **kwargs: Extra keyword arguments for Dask from_zarr.
 
     Returns:
         Zarr array opened with Dask engine.
     """
     zarr_array = open_zarr_array(group_handle=group_handle, name=name)
-    return da.from_array(zarr_array, **kwargs, inline_array=True)
+    return from_array(zarr_array, **kwargs, inline_array=True)
