046a73cd5c9d5a03002313b45e3a91511a8f1143
BrianMichell <brianm314@comcast.net>
2025-05-27 15:19:58 +0000

    Fix test schema from snake to camel case

--- tests/unit/test_schema.py
+++ tests/unit/test_schema.py
@@ -5,43 +5,43 @@ from mdio.schemas.v1 import Dataset as V1Dataset
 TEST_SCHEMA = {
     "metadata": {
         "name": "test_dataset",
-        "api_version": "1.0.0",
-        "created_on": "2023-01-01T00:00:00Z",
+        "apiVersion": "1.0.0",
+        "createdOn": "2023-01-01T00:00:00Z",
     },
     "variables": [
         {
             "name": "actual_variable",
-            "data_type": "float32",
+            "dataType": "float32",
             "dimensions": ["dim0", "dim1"],
             "compressor": {"name": "blosc", "level": 3},
             "coordinates": ["coord"],
             "metadata": {
-                "chunk_grid": {
+                "chunkGrid": {
                     "name": "regular",
-                    "configuration": {"chunk_shape": [10, 20]},
+                    "configuration": {"chunkShape": [10, 20]},
                 },
             },
         },
         {
             "name": "coord",
-            "data_type": "float32",
+            "dataType": "float32",
             "dimensions": ["dim0", "dim1"],
             "metadata": {
-                "chunk_grid": {
+                "chunkGrid": {
                     "name": "regular",
-                    "configuration": {"chunk_shape": [10, 20]},
+                    "configuration": {"chunkShape": [10, 20]},
                 },
-                "units_v1": {"length": "m"},
+                "unitsV1": {"length": "m"},
             },
         },
         {
             "name": "dim0",
-            "data_type": "int32",
+            "dataType": "int32",
             "dimensions": [{"name": "dim0", "size": 100}],
         },
         {
             "name": "dim1",
-            "data_type": "int32",
+            "dataType": "int32",
             "dimensions": [{"name": "dim1", "size": 200}],
         },
     ],
