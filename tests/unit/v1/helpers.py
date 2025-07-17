04476597f92721ccf2c040caba9c1e402ba5cea7
Dmitriy Repin <drepin@hotmail.com>
2025-07-17 15:50:33 +0000

    Fix spelling

--- tests/unit/v1/helpers.py
+++ tests/unit/v1/helpers.py
@@ -129,7 +129,7 @@ def _get_coordinate(
 
 
 def _get_all_coordinates(dataset: Dataset) -> list[Coordinate]:
-    """Get all coordinates from the dataset."""
+    """Get all Coordinates from the Dataset."""
     all_coords: dict[str, Coordinate] = {}
     for v in dataset.variables:
         if v.coordinates is not None:
