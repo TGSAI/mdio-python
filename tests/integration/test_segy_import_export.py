63737a6716c3d1b9a358517bb31face0cbaa9f7a
Altay Sansal <tasansal@users.noreply.github.com>
2025-08-07 19:52:08 -0500

    clean up whitespace

--- tests/integration/test_segy_import_export.py
+++ tests/integration/test_segy_import_export.py
@@ -12,21 +12,22 @@ import numpy.testing as npt
 import pytest
 import xarray as xr
 from segy import SegyFile
-from segy.schema import SegySpec
 from segy.standards import get_segy_standard
-from tests.integration.testing_helpers import customize_segy_specs, get_inline_header_values, get_values, validate_variable
+from tests.integration.testing_helpers import customize_segy_specs
+from tests.integration.testing_helpers import get_inline_header_values
+from tests.integration.testing_helpers import get_values
+from tests.integration.testing_helpers import validate_variable
 
 from mdio import MDIOReader
 from mdio import mdio_to_segy
 from mdio.converters import segy_to_mdio
 from mdio.converters.exceptions import GridTraceSparsityError
-from mdio.converters.segy_to_mdio_v1 import segy_to_mdio_v1
+from mdio.converters.segy_v1 import segy_to_mdio_v1
 from mdio.core import Dimension
 from mdio.core.storage_location import StorageLocation
 from mdio.schemas.v1.templates.template_registry import TemplateRegistry
 from mdio.segy.compat import mdio_segy_spec
 from mdio.segy.geometry import StreamerShotGeometryType
-from tests.integration.v1.test_segy_to_mdio_v1 import _validate_variable
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -282,7 +283,6 @@ def test_3d_import_v1(
         output_location=StorageLocation(zarr_tmp.__str__()),
         overwrite=True,
     )
-    pass
 
 
 @pytest.mark.dependency("test_3d_import")
@@ -294,7 +294,11 @@ class TestReader:
         path = zarr_tmp.__str__()
         # path = "/tmp/pytest-of-vscode/my-mdio/mdio0"
         ds = xr.open_dataset(path, engine="zarr")
-        expected_attrs = {'apiVersion': '1.0.0a1', 'createdOn': '2025-08-06 16:21:54.747880+00:00', 'name': 'PostStack3DTime'}
+        expected_attrs = {
+            "apiVersion": "1.0.0a1",
+            "createdOn": "2025-08-06 16:21:54.747880+00:00",
+            "name": "PostStack3DTime",
+        }
         actual_attrs_json = ds.attrs
         # compare one by one due to ever changing createdOn. For it, we only check existence
         for key, value in expected_attrs.items():
@@ -310,11 +314,15 @@ class TestReader:
         # path = "/tmp/pytest-of-vscode/my-mdio/mdio0"
         ds = xr.open_dataset(path, engine="zarr")
         expected_attrs = {
-            "count": 97354860, "sum": -8594.551666259766, "sum_squares": 40571291.6875,
-            "min": -8.375323295593262, "max": 0.0, "histogram": {"counts": [], "bin_centers": []}}
-        actual_attrs_json =  json.loads(ds["amplitude"].attrs['statsV1'])
+            "count": 97354860,
+            "sum": -8594.551666259766,
+            "sum_squares": 40571291.6875,
+            "min": -8.375323295593262,
+            "max": 0.0,
+            "histogram": {"counts": [], "bin_centers": []},
+        }
+        actual_attrs_json = json.loads(ds["amplitude"].attrs["statsV1"])
         assert actual_attrs_json == expected_attrs
-
 
     def test_grid(self, zarr_tmp: Path) -> None:
         """Test validating MDIO variables."""
@@ -323,14 +331,16 @@ class TestReader:
         # path = "/tmp/pytest-of-vscode/my-mdio/mdio0"
         ds = xr.open_dataset(path, engine="zarr")
 
-        # Note: in order to create the dataset we used the Time template, so the 
+        # Note: in order to create the dataset we used the Time template, so the
         # sample dimension is called "time"
 
         # Validate the dimension coordinate variables
         validate_variable(ds, "inline", (345,), ["inline"], np.int32, range(1, 346), get_values)
-        validate_variable(ds, "crossline", (188,), ["crossline"], np.int32, range(1, 189), get_values)
+        validate_variable(
+            ds, "crossline", (188,), ["crossline"], np.int32, range(1, 189), get_values
+        )
         validate_variable(ds, "time", (1501,), ["time"], np.int64, range(0, 3002, 2), get_values)
-    
+
         # Validate the non-dimensional coordinate variables
         validate_variable(ds, "cdp_x", (345, 188), ["inline", "crossline"], np.int32, None, None)
         validate_variable(ds, "cdp_y", (345, 188), ["inline", "crossline"], np.int32, None, None)
@@ -338,17 +348,34 @@ class TestReader:
         # Validate the headers
         # We have a subset of headers since we used customize_segy_specs() providing the values only
         # for "inline", "crossline", "cdp_x", "cdp_y"
-        data_type = np.dtype([('inline', '<i4'), ('crossline', '<i4'), ('cdp_x', '<i4'), ('cdp_y', '<i4')])
-        validate_variable(ds, "headers", (345, 188), ["inline", "crossline"], data_type,
-                          range(1, 346), get_inline_header_values)
-        
+        data_type = np.dtype(
+            [("inline", "<i4"), ("crossline", "<i4"), ("cdp_x", "<i4"), ("cdp_y", "<i4")]
+        )
+        validate_variable(
+            ds,
+            "headers",
+            (345, 188),
+            ["inline", "crossline"],
+            data_type,
+            range(1, 346),
+            get_inline_header_values,
+        )
+
         # Validate the trace mask
-        validate_variable(ds, "trace_mask", (345, 188), ["inline", "crossline"], np.bool, None, None)
+        validate_variable(
+            ds, "trace_mask", (345, 188), ["inline", "crossline"], np.bool, None, None
+        )
 
         # validate the amplitude data
-        validate_variable(ds, "amplitude", (345, 188, 1501), ["inline", "crossline", "time"], 
-                          np.float32, None, None)
-
+        validate_variable(
+            ds,
+            "amplitude",
+            (345, 188, 1501),
+            ["inline", "crossline", "time"],
+            np.float32,
+            None,
+            None,
+        )
 
     def test_inline(self, zarr_tmp: Path) -> None:
         """Read and compare every 75 inlines' mean and std. dev."""
@@ -358,7 +385,6 @@ class TestReader:
         inlines = ds["amplitude"][::75, :, :]
         mean, std = inlines.mean(), inlines.std()
         npt.assert_allclose([mean, std], [1.0555277e-04, 6.0027051e-01])
-        pass
 
     def test_crossline(self, zarr_tmp: Path) -> None:
         """Read and compare every 75 crosslines' mean and std. dev."""
@@ -375,7 +401,7 @@ class TestReader:
         path = zarr_tmp.__str__()
         # path = "/tmp/pytest-of-vscode/my-mdio/mdio0"
         ds = xr.open_dataset(path, engine="zarr")
-        slices =  ds["amplitude"][:, :, ::225]
+        slices = ds["amplitude"][:, :, ::225]
         mean, std = slices.mean(), slices.std()
         npt.assert_allclose([mean, std], [0.005236923, 0.61279935])
 
