410c2b626d78ee3e4ede5a9e2b1116e974625aa9
Altay Sansal <tasansal@users.noreply.github.com>
2025-08-22 08:50:23 -0500

    Refactor `Seismic2DPreStackCDPTemplate` to update `chunk_shape`, add access patterns, and include `ChunkGridMetadata` for coordinate dimensions.

--- src/mdio/schemas/v1/templates/seismic_2d_prestack_cdp.py
+++ src/mdio/schemas/v1/templates/seismic_2d_prestack_cdp.py
@@ -1,11 +1,20 @@
 """Seismic2DPreStackCDPTemplate MDIO v1 dataset templates."""
 
+from mdio.schemas import ScalarType
+from mdio.schemas.chunk_grid import RegularChunkGrid
+from mdio.schemas.chunk_grid import RegularChunkShape
+from mdio.schemas.metadata import ChunkGridMetadata
 from mdio.schemas.metadata import UserAttributes
 from mdio.schemas.v1.templates.abstract_dataset_template import AbstractDatasetTemplate
 
 
 class Seismic2DPreStackCDPTemplate(AbstractDatasetTemplate):
-    """Seismic CDP pre-stack 2D time or depth Dataset template."""
+    """Seismic CDP pre-stack 2D time or depth Dataset template.
+
+    Access Patterns:
+        cdp: full gathers
+        offset: offset planes
+    """
 
     def __init__(self, domain: str):
         super().__init__(domain=domain)
@@ -13,7 +22,7 @@ class Seismic2DPreStackCDPTemplate(AbstractDatasetTemplate):
         self._coord_dim_names = ["cdp", "offset"]
         self._dim_names = [*self._coord_dim_names, self._trace_domain]
         self._coord_names = ["cdp_x", "cdp_y"]
-        self._var_chunk_shape = [1, 512, 4096]
+        self._var_chunk_shape = (16, 64, 1024)
 
     @property
     def _name(self) -> str:
@@ -26,4 +35,31 @@ class Seismic2DPreStackCDPTemplate(AbstractDatasetTemplate):
                 "ensembleType": "cdp",
                 "processingStage": "pre-stack",
             }
+        )
+
+    def _add_coordinates(self) -> None:
+        # Add dimension coordinates
+        for name in self._dim_names:
+            self._builder.add_coordinate(
+                name,
+                dimensions=[name],
+                data_type=ScalarType.INT32,
+                metadata_info=None,
+            )
+
+        # Add non-dimension coordinates
+        coord_chunk_shape = (131072,)  # 1MB with fp64
+        chunk_grid = RegularChunkGrid(configuration=RegularChunkShape(chunk_shape=coord_chunk_shape))
+        chunk_meta = ChunkGridMetadata(chunk_grid=chunk_grid)
+        self._builder.add_coordinate(
+            "cdp_x",
+            dimensions=["cdp"],
+            data_type=ScalarType.FLOAT64,
+            metadata_info=[self._horizontal_coord_unit, chunk_meta],
+        )
+        self._builder.add_coordinate(
+            "cdp_y",
+            dimensions=["cdp"],
+            data_type=ScalarType.FLOAT64,
+            metadata_info=[self._horizontal_coord_unit, chunk_meta],
         )
