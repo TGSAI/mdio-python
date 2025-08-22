605f689be21fe1417635ae73a18a26a0c86d26bb
Altay Sansal <tasansal@users.noreply.github.com>
2025-08-22 08:50:16 -0500

    Refactor `Seismic2DPreStackShotTemplate` to update `chunk_shape`, access patterns, and coordinate dimensions

--- src/mdio/schemas/v1/templates/seismic_2d_prestack_shot.py
+++ src/mdio/schemas/v1/templates/seismic_2d_prestack_shot.py
@@ -7,7 +7,11 @@ from mdio.schemas.v1.units import AllUnits
 
 
 class Seismic2DPreStackShotTemplate(AbstractDatasetTemplate):
-    """Seismic Shot pre-stack 2D time or depth Dataset template."""
+    """Seismic Shot pre-stack 2D time or depth Dataset template.
+
+    Access Patterns:
+        shot_point: full shots
+    """
 
     def __init__(self, domain: str):
         super().__init__(domain=domain)
@@ -15,7 +19,7 @@ class Seismic2DPreStackShotTemplate(AbstractDatasetTemplate):
         self._coord_dim_names = ["shot_point", "channel"]  # Custom coordinate definition for shot gathers
         self._dim_names = [*self._coord_dim_names, self._trace_domain]
         self._coord_names = ["gun", "source_coord_x", "source_coord_y", "group_coord_x", "group_coord_y"]
-        self._var_chunk_shape = [1, 512, 4096]
+        self._var_chunk_shape = (8, 256, 1024)
 
     @property
     def _name(self) -> str:
@@ -25,7 +29,7 @@ class Seismic2DPreStackShotTemplate(AbstractDatasetTemplate):
         return UserAttributes(
             attributes={
                 "surveyDimensionality": "2D",
-                "ensembleType": "shot_point",
+                "ensembleType": "shot",
                 "processingStage": "pre-stack",
             }
         )
@@ -41,21 +45,22 @@ class Seismic2DPreStackShotTemplate(AbstractDatasetTemplate):
             )
 
         # Add non-dimension coordinates
+        # TODO: To chunk or not to chunk. Chunk sizes ??
         self._builder.add_coordinate(
             "gun",
-            dimensions=["shot_point", "channel"],
+            dimensions=["shot_point"],
             data_type=ScalarType.UINT8,
             metadata_info=[AllUnits(units_v1=None)],
         )
         self._builder.add_coordinate(
             "source_coord_x",
-            dimensions=["shot_point", "channel"],
+            dimensions=["shot_point"],
             data_type=ScalarType.FLOAT64,
             metadata_info=[self._horizontal_coord_unit],
         )
         self._builder.add_coordinate(
             "source_coord_y",
-            dimensions=["shot_point", "channel"],
+            dimensions=["shot_point"],
             data_type=ScalarType.FLOAT64,
             metadata_info=[self._horizontal_coord_unit],
         )
