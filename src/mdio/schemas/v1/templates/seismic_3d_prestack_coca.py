0d598f42dadadccd41b4282a78b3d8ea1f9434d8
Altay Sansal <tasansal@users.noreply.github.com>
2025-08-22 08:49:27 -0500

    Refactor `Seismic3DPreStackCocaTemplate` to use tuple `chunk_shape` and include `ChunkGridMetadata` for coordinates.

--- src/mdio/schemas/v1/templates/seismic_3d_prestack_coca.py
+++ src/mdio/schemas/v1/templates/seismic_3d_prestack_coca.py
@@ -1,11 +1,15 @@
 """Seismic3DPreStackCocaTemplate MDIO v1 dataset templates."""
 
+from mdio.schemas.chunk_grid import RegularChunkGrid
+from mdio.schemas.chunk_grid import RegularChunkShape
 from mdio.schemas.dtype import ScalarType
+from mdio.schemas.metadata import ChunkGridMetadata
 from mdio.schemas.metadata import UserAttributes
 from mdio.schemas.v1.templates.abstract_dataset_template import AbstractDatasetTemplate
 from mdio.schemas.v1.units import AllUnits
 
 
+# TODO: This shouldn't exist for 2D, confirm
 class Seismic3DPreStackCocaTemplate(AbstractDatasetTemplate):
     """Seismic Shot pre-stack 3D time or depth Dataset template."""
 
@@ -15,7 +19,7 @@ class Seismic3DPreStackCocaTemplate(AbstractDatasetTemplate):
         self._coord_dim_names = ["inline", "crossline", "offset", "azimuth"]
         self._dim_names = [*self._coord_dim_names, self._trace_domain]
         self._coord_names = ["cdp_x", "cdp_y"]
-        self._var_chunk_shape = [8, 8, 32, 1, 1024]
+        self._var_chunk_shape = (8, 8, 32, 1, 1024)
 
     @property
     def _name(self) -> str:
@@ -62,15 +66,18 @@ class Seismic3DPreStackCocaTemplate(AbstractDatasetTemplate):
         )
 
         # Add non-dimension coordinates
+        coord_chunk_shape = (384, 384)  # 1.1MB with fp64
+        chunk_grid = RegularChunkGrid(configuration=RegularChunkShape(chunk_shape=coord_chunk_shape))
+        chunk_meta = ChunkGridMetadata(chunk_grid=chunk_grid)
         self._builder.add_coordinate(
             "cdp_x",
             dimensions=["inline", "crossline"],
             data_type=ScalarType.FLOAT64,
-            metadata_info=[self._horizontal_coord_unit],
+            metadata_info=[self._horizontal_coord_unit, chunk_meta],
         )
         self._builder.add_coordinate(
             "cdp_y",
             dimensions=["inline", "crossline"],
             data_type=ScalarType.FLOAT64,
-            metadata_info=[self._horizontal_coord_unit],
+            metadata_info=[self._horizontal_coord_unit, chunk_meta],
         )
