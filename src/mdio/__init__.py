3fefea40254b491eb753a166fd06910ab24307e4
Dmitriy Repin <drepin@hotmail.com>
2025-10-29 20:30:53 +0000

    pre-commit

--- src/mdio/__init__.py
+++ src/mdio/__init__.py
@@ -7,12 +7,12 @@ try:
 except metadata.PackageNotFoundError:
     __version__ = "unknown"
 
+from mdio.api.create import create_empty
+from mdio.api.create import create_empty_like
 from mdio.api.io import open_mdio
 from mdio.api.io import to_mdio
 from mdio.converters.mdio import mdio_to_segy
 from mdio.converters.segy import segy_to_mdio
-from mdio.api.create import create_empty
-from mdio.api.create import create_empty_like
 
 __all__ = [
     "__version__",
