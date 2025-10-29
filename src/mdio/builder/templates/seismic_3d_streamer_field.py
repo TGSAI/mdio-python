28ee56b2ec067c1f79628c933b2f54e09b9ebd9e
Altay Sansal <13684161+tasansal@users.noreply.github.com>
2025-10-29 14:28:37 -0500

    rename streamer field data template

--- src/mdio/builder/templates/seismic_3d_streamer_field.py
+++ src/mdio/builder/templates/seismic_3d_streamer_field.py
@@ -1,4 +1,4 @@
-"""SeismicPreStackTemplate MDIO v1 dataset templates."""
+"""Seismic3DStreamerFieldRecordsTemplate MDIO v1 dataset templates."""
 
 from typing import Any
 
@@ -8,7 +8,7 @@ from mdio.builder.templates.base import AbstractDatasetTemplate
 from mdio.builder.templates.types import SeismicDataDomain
 
 
-class Seismic3DPreStackStreamerFieldRecordsTemplate(AbstractDatasetTemplate):
+class Seismic3DStreamerFieldRecordsTemplate(AbstractDatasetTemplate):
     """Seismic pre-stack time Dataset template.
 
     A generalized template for pre-stack field records in either 2D or 3D.
@@ -19,26 +19,23 @@ class Seismic3DPreStackStreamerFieldRecordsTemplate(AbstractDatasetTemplate):
         data_domain: The domain of the dataset.
     """
 
-    def __init__(self, data_domain: SeismicDataDomain):
+    def __init__(self, data_domain: SeismicDataDomain = "time"):
         super().__init__(data_domain=data_domain)
 
         self._spatial_dim_names = ("shot_line", "gun", "shot_point", "cable", "channel")
         self._dim_names = (*self._spatial_dim_names, self._data_domain)
         self._physical_coord_names = ("source_coord_x", "source_coord_y", "group_coord_x", "group_coord_y")
-        self._logical_coord_names = ("orig_field_record_num",)
-        # TODO(Anyone): Disable chunking in time domain when support is merged.
-        # https://github.com/TGSAI/mdio-python/pull/723
-        # self._var_chunk_shape = (1, 1, 16, 1, 32, -1)
+        self._logical_coord_names = ("orig_field_record_num",)  # ffid
         self._var_chunk_shape = (1, 1, 16, 1, 32, 1024)
 
     @property
     def _name(self) -> str:
-        return f"PreStackStreamerFieldRecords3D{self._data_domain.capitalize()}"
+        return "StreamerFieldRecords3D"
 
     def _load_dataset_attributes(self) -> dict[str, Any]:
         return {
             "surveyDimensionality": "3D",
-            "ensembleType": "shot_point",
+            "ensembleType": "track",
             "processingStage": "pre-stack",
         }
 
