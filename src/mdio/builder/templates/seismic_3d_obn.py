c1b6dd41f7f87b4852bf0993cfd6744893ca4104
BrianMichell <brianm314@comcast.net>
2026-01-20 14:18:41 +0000

    Update default chunking

--- src/mdio/builder/templates/seismic_3d_obn.py
+++ src/mdio/builder/templates/seismic_3d_obn.py
@@ -23,7 +23,7 @@ class Seismic3DObnReceiverGathersTemplate(AbstractDatasetTemplate):
             "source_coord_y",
         )
         self._logical_coord_names = ("orig_field_record_num",)
-        self._var_chunk_shape = (4, 8, 1, 2, 8, 4096)
+        self._var_chunk_shape = (1, 1, 1, 3, 128, 4096)
 
     @property
     def _name(self) -> str:
--- src/mdio/builder/templates/seismic_3d_obn_single_component.py
+++ src/mdio/builder/templates/seismic_3d_obn_single_component.py
@@ -23,7 +23,7 @@ class Seismic3DObnSingleComponentGathersTemplate(AbstractDatasetTemplate):
             "source_coord_y",
         )
         self._logical_coord_names = ("orig_field_record_num",)
-        self._var_chunk_shape = (16, 1, 2, 16, 4096)
+        self._var_chunk_shape = (1, 1, 3, 128, 4096)
 
     @property
     def _name(self) -> str:
